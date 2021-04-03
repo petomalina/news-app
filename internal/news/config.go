9547c8a64b1784d7f6759ecd0f4951acc50f3e37
Peter Malina <peter.malina@flowup.cz>
2021-04-03 10:09:38 +0100

    docs(news): added missing docs to config struct and provider iface

--- internal/news/config.go
+++ internal/news/config.go
@@ -1,5 +1,9 @@
 package news
 
+// Config is the news server configuration that can be passed
+// in either directly or via an env+default configuration.
+// This struct uses https://github.com/sethvargo/go-envconfig
+// to process the incoming env variables.
 type Config struct {
 	Port string `env:"PORT,default=8080"`
 }
--- internal/news/feed/provider.go
+++ internal/news/feed/provider.go
@@ -1,5 +1,6 @@
 package feed
 
+// Provider abstracts an Article provider for a feed
 type Provider interface {
 	Fetch(category string) ([]*Article, error)
 }
